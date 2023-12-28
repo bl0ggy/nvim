1c65630290480b1522a7f9c903096b2721603a03
Jessy Simeon <jessys34@hotmail.fr>
2023-12-28 11:33:05 +0900

    Set working dir at startup

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
 require("thejs")
+-- Set working dir to the current dir at startup (e.g. `nvim subfolder` sets the working to 'subfolder' instead of '.')
+vim.cmd(":cd" .. vim.api.nvim_buf_get_name(0))
