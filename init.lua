5056734a6ce1f1b3b8f1115ecb3ff185b64fd6f9
Jessy Simeon <jessys34@hotmail.fr>
2024-01-04 14:40:35 +0900

    Fix config when opening a file

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 require("thejs")
 -- Set working dir to the current dir at startup (e.g. `nvim subfolder` sets the working to 'subfolder' instead of '.')
-vim.cmd(":cd" .. vim.api.nvim_buf_get_name(0))
+
+local isDir = vim.fn.isdirectory(vim.api.nvim_buf_get_name(0))
+if isDir == 1 then
+    vim.cmd(":cd" .. vim.api.nvim_buf_get_name(0))
+end
