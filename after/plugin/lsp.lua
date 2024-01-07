6ed05f91b62711640d5aab176424e796dbaf5d45
Jessy Simeon <jessys34@hotmail.fr>
2024-01-07 19:23:58 +0100

    Fix formatting conflict

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,7 +7,6 @@ lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function()
         vim.lsp.buf.definition()
-        print("haha")
     end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -18,6 +17,33 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    -- How to avoid formatting conflict
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
+    local augroup = vim.api.nvim_create_augroup('MyLspFormatOnSaveAll', {})
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({
+                bufnr = bufnr,
+                filter = function(client_)
+                    -- apply whatever logic you want (in this example, we'll only use null-ls)
+                    local bufname = vim.api.nvim_buf_get_name(bufnr)
+                    local extjs = bufname:match(".*%.js$")
+                    local extts = bufname:match(".*%.ts$")
+                    local extjsx = bufname:match(".*%.jsx$")
+                    local exttsx = bufname:match(".*%.tsx$")
+                    if (extjs or extts or extjsx or exttsx) then
+                        -- Uses formatter in after/plugin/eslint.lua
+                        return
+                    end
+                    return client_.name
+                end,
+            })
+        end,
+    })
 end)
 
 require('mason').setup({})
--- lua/thejs/init.lua
+++ lua/thejs/init.lua
@@ -3,10 +3,3 @@ require("thejs.set")
 require("thejs.packer")
 
 vim.diagnostic.config({ virtual_text = true, update_in_insert = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = { '*' },
-    group = vim.api.nvim_create_augroup('MyFormatOnSaveAll', {}),
-    callback = function()
-        vim.lsp.buf.format()
-    end
-})
